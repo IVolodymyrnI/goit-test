36a80b3aed3d17a5e71cc5fff5ede18e48de2b0c
IVolodymyrnI <volodia.iarcguk.forwork@gmail.com>
2023-05-07 18:16:48 +0300

    FIXED: path of tweets` page

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
     <Routes>
       <Route path="/" element={<SharedLayout />}>
         <Route index element={<Home />} />
-        <Route path="/tweets" element={<Tweets />} />
+        <Route path="tweets" element={<Tweets />} />
         <Route path="*" element={<Home />} />
       </Route>
     </Routes>
